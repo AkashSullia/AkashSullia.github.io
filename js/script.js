e75746ae9e6a87c3e88726f759122817e8982285
Akash Sullia <akash2514@gmail.com>
2020-05-31 22:29:13 +0530

    Added modal on Our Team. Saving snapshot before background.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
-const btn = document.getElementById('cta-btn');
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
-btn.addEventListener('click', () => {
-    overlay.style.display = 'grid';
-    overlay.classList.add('animate-overlay');
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
 });
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => closeModal(modal));
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
+const openModal = (modal) => {
+    if (modal == null) {
+        return;
+    }
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+const closeModal = (modal) => {
+    if (modal == null) {
+        return;
+    }
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
